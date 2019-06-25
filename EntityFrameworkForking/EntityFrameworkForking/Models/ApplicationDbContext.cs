f095fe1db38261efa6baeb13c0f79960c583af85
Unknown <19vign@elmbrookstudents.org>
2019-06-25 12:05:10 -0500

    init

---
+++ EntityFrameworkForking/EntityFrameworkForking/Models/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+using System.Reflection;
+using EntityFrameworkForking.Models.Entity;
+using EntityFrameworkForking.Models;
+
+namespace DatabaseActivities.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+
+        }
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.AddFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ EntityFrameworkForking/EntityFrameworkForking/Models/Entity/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameworkForking.Models.Entity
+{
+    public class Student :IEntity
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string teacher { get; set; }
+    }
+}
